e8be6817e17926336d8d0cfd78ceb320d3f98729
Robert Fitzpatrick <rob@affiliatebrothers.com>
2025-02-27 20:25:18 -0700

    Set up complete React application structure for Netlify deployment

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import PortfolioCalculator from './PortfolioCalculator';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <PortfolioCalculator />
+  </React.StrictMode>
+);
